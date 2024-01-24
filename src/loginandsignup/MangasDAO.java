e72d11886919ede9395b4ae9eb7a3a7d1ae503f1
HibaZouhair <hibazhr200@gmail.com>
2024-01-24 20:01:58 +0100

    commit

--- src/loginandsignup/MangasDAO.java
+++ src/loginandsignup/MangasDAO.java
@@ -6,7 +6,11 @@ package loginandsignup;
 
 
 
- import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.LinkedList;
 import java.util.List;
 
